646668f559b169d64e57713728bf0c87e62a05d9
Abiodun <abiodunojo453@gmail.com>
2025-05-19 17:27:43 +0100

    further config

---
+++ README.md
@@ -0,0 +1,26 @@
+# 💸 React Native User Authentication
+
+Demo user authentication app built with **React Native (Expo)** based on Firebase.
+
+---
+
+## 📱 Features
+
+- User Authentication
+- Password Validation
+- Demo
+- Environment variables using `.env` and `@env`
+- Android APK build ready (via EAS)
+
+---
+
+## 🔧 Tech Stack
+
+- React Native (with Expo)
+- Axios for API requests
+- Firebase Authentication
+- EAS Build for generating APKs
+
+---
+## ✨ Author
+- Abiodun (Phenomenal Productions)
--- app.config.js
+++ app.config.js
@@ -1,15 +1,23 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 export default {
   expo: {
     name: "User Auth",
-    slug: "user-auth",
+    slug: "your-app-slug",
     version: "1.0.0",
     extra: {
-        "eas": {
-        "projectId": "a8f7d1c8-4e0a-4f94-b291-f94704d13099"
+      eas: {
+        projectId: "a8f7d1c8-4e0a-4f94-b291-f94704d13099",
       },
-      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY,
+      FIREBASE_API_KEY: process.env.FIREBASE_API_KEY, // API key from .env file
+    },
+    android: {
+      package: "com.phenomenalproductions.userauth",
+      versionCode: 1,
+      adaptiveIcon: {
+        foregroundImage: "./assets/icon.png",
+        backgroundColor: "#FFFFFF",
+      },
     },
   },
 };
